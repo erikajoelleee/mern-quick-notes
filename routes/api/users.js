caa0070e0fdfdcc0da113ec406e8524328bdc34a
Erika Roncal-Schuyler <Roncal.schuyler@gmail.com>
2023-09-05 21:34:24 -0400

    quick notes commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,44 @@
-const express = require('express');
-const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
-const ensureLoggedIn = require('../../config/ensureLoggedIn');
+import { useState } from 'react';
+import * as usersService from '../../utilities/users-service';
 
-// All paths start with '/api/users'
+export default function LoginForm({ setUser }) {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState('');
 
-// POST /api/users (create a user - sign up)
-router.post('/', usersCtrl.create);
-// POST /api/users/login
-router.post('/login', usersCtrl.login);
-// GET /api/users/check-token
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+  function handleChange(evt) {
+    setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
+    setError('');
+  }
 
-module.exports = router;
+  async function handleSubmit(evt) {
+    // Prevent form from being submitted to the server
+    evt.preventDefault();
+    try {
+      // The promise returned by the signUp service method 
+      // will resolve to the user object included in the
+      // payload of the JSON Web Token (JWT)
+      const user = await usersService.login(credentials);
+      setUser(user);
+    } catch {
+      setError('Log In Failed - Try Again');
+    }
+  }
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
+          <label>Password</label>
+          <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+          <button type="submit">LOG IN</button>
+        </form>
+      </div>
+      <p className="error-message">&nbsp;{error}</p>
+    </div>
+  );
+}
